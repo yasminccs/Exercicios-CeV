eb64b331e96a70fcf06dfbaac06d518e9a9a7979
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2022-10-09 19:34:27 -0300

    new exercise

--- JavaScript/Funções/Exercício 1/script.js
+++ JavaScript/Funções/Exercício 1/script.js
@@ -14,9 +14,25 @@ function addNumber(){
         options.innerHTML += `Valor ${input} adicionado.`
         res.appendChild(options)
     }
+    let result = document.querySelector('#result').innerHTML = ''
+    nmr.value = ''
+    nmr.focus()
 }
 
 function final(){
-    const result = document.querySelector('#result')
-    result.innerHTML = `Ao todo, temos ${arr.length} números cadastrados.`
+    let op = document.querySelector('option')
+    if(arr.length === 0){
+        alert('Adicione um número antes de finalizar!')
+    } else {
+        const result = document.querySelector('#result')
+        result.innerHTML += `Ao todo, temos ${arr.length} número(s) cadastrado(s).<br>`
+        result.innerHTML += `O maior valor digitado foi ${Math.max(...arr)}.<br>`
+        result.innerHTML += `O menor valor digitado foi ${Math.min(...arr)}.<br>`
+        let sum = arr.reduce((acc, element) => {
+            return acc + element
+        }, 0)
+        result.innerHTML += `Somando todos os valores temos: ${sum}.<br>`
+        let media = sum / arr.length
+        result.innerHTML += `A média dos valores digitados é ${Math.round(media)}.<br>`
+    }
 }
